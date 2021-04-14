edba5b71fe1165a69628e91e400c5357ec5aeb0c
Cho Won Jae <yoteamo7@naver.com>
2021-04-15 00:55:03 +0900

    Blog 관련 API 구현(GET,PUT,PATCH)

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -4,6 +4,9 @@ import Blog from '../models/Blog';
 
 export const getBlog = async (req, res, next) => {
   try {
+    const blogs = await Blog.find({});
+
+    return res.status(200).json({ blogs });
   } catch (error) {
     next(error);
   }
@@ -11,6 +14,14 @@ export const getBlog = async (req, res, next) => {
 
 export const getSpecificBlog = async (req, res, next) => {
   try {
+    const {
+      params: { blogId },
+    } = req;
+
+    if (!mongoose.isValidObjectId(blogId)) return res.status(400).send({ err: 'blogId is invalid' });
+
+    const blog = await Blog.findById(blogId);
+    return res.status(200).json({ blog });
   } catch (error) {
     next(error);
   }
@@ -55,6 +66,26 @@ export const postBlog = async (req, res, next) => {
 
 export const putBlog = async (req, res, next) => {
   try {
+    const {
+      body: { title, content },
+      params: { blogId },
+    } = req;
+
+    if (!mongoose.isValidObjectId(blogId)) return res.status(400).send({ err: 'blogId is invalid' });
+    if (!title) {
+      return res.status(400).send({ err: 'title is required' });
+    } else {
+      if (typeof title !== 'string') return res.status(400).send({ err: 'title must be a String' });
+    }
+
+    if (!content) {
+      return res.status(400).send({ err: 'content is required' });
+    } else {
+      if (typeof content !== 'string') return res.status(400).send({ err: 'content must be a String' });
+    }
+
+    const blog = await Blog.findByIdAndUpdate(blogId, req.body, { new: true });
+    return res.status(200).json({ blog });
   } catch (error) {
     next(error);
   }
@@ -62,6 +93,20 @@ export const putBlog = async (req, res, next) => {
 
 export const patchBlog = async (req, res, next) => {
   try {
+    const {
+      body: { isLive },
+      params: { blogId },
+    } = req;
+
+    if (!mongoose.isValidObjectId(blogId)) return res.status(400).send({ err: 'blogId is invalid' });
+    if (!isLive) {
+      return res.status(400).send({ err: 'isLive is required' });
+    } else {
+      if (typeof isLive !== 'boolean') return res.status(400).send({ err: 'isLive must be a boolean' });
+    }
+
+    const blog = await Blog.findByIdAndUpdate(blogId, req.body, { new: true });
+    return res.status(200).json({ blog });
   } catch (error) {
     next(error);
   }
